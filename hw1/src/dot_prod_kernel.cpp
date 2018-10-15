e815d3515c0a72bbef4c00a596922adad5f77817
Cloud User <centos@ip-172-31-12-120.us-east-2.compute.internal>
2018-10-15 07:53:03 +0000

    hw1: fouth version

--- hw1/src/dot_prod_kernel.cpp
+++ hw1/src/dot_prod_kernel.cpp
@@ -20,20 +20,18 @@ void dot_prod_kernel(const float* a, const float* b, float* c, const int num_ele
    float temp = 0;
 
    float local_a[4096];
-#pragma HLS ARRAY_PARTITION variable=local_a block factor=128 dim=1
+#pragma HLS ARRAY_PARTITION variable=local_a block factor=512 dim=1
    float local_b[4096];
-#pragma HLS ARRAY_PARTITION variable=local_b block factor=128 dim=1
-   float local_c[4096];
-#pragma HLS ARRAY_PARTITION variable=local_c block factor=128 dim=1
-
+#pragma HLS ARRAY_PARTITION variable=local_b block factor=512 dim=1
+   float local_c[4097];
+#pragma HLS ARRAY_PARTITION variable=local_c block factor=512 dim=1
+   local_c[0] = 0;
 #pragma HLS INLINE OFF
 for(i = 0; i<num_elems; i++)
 {
 #pragma HLS UNROLL 
 #pragma HLS PIPELINE II=1
-      local_a[i] = a[i];
-        
-
+      local_a[i] = a[i];        
 }
 
 for(i = 0; i<num_elems; i++)
@@ -47,24 +45,29 @@ for(i = 0; i<num_elems; i++)
 
 for (j=0;j<num_elems;j=j+32)
 {
-#pragma HLS loop_tripcount min=0 max=128
-#pragma HLS UNROLL 
+
+#pragma HLS loop_tripcount min=0 max=128 
+#pragma HLS UNROLL
 
 for (i=j; i<j+32; i++)
 {
 #pragma HLS loop_tripcount min=0 max=32
 #pragma HLS PIPELINE II=1
-local_c[i] = local_a[i] * local_b[i];
+local_c[i+1] = local_c[i] + local_a[i] * local_b[i];
 }
 
+temp += local_c[i];
+local_c[i] = 0;
 }
+/*
 for(i=0;i<num_elems;i++)
 {
-#pragma HLS loop_tripcount min=0 max=32
+//#pragma HLS loop_tripcount min=0 max=32
+#pragma HLS UNROLL
 #pragma HLS PIPELINE II=1
 temp+=local_c[i];
 }
-
+*/
    *c = temp;
 
 }
